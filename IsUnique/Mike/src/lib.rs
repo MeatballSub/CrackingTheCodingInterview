08a485830ab0369cafc9a3d51cb7830853f95961
mtwatson <watsonmichael919@gmail.com>
2025-11-01 10:07:25 -0500

    mtw solutions

--- IsUnique/Mike/src/lib.rs
+++ IsUnique/Mike/src/lib.rs
@@ -1,7 +1,42 @@
 pub mod test;
 
+//faster, more memory intensive
 pub fn is_unique(string: &str) -> bool {
-    todo!("Implement this function")
+
+    if string.chars().count() > 128  {
+        return false
+    }
+
+    const limit: usize = 128;
+    let mut charHash = [false; limit];
+
+    for c in string.chars() {
+        let charCode = u32::from(c);
+
+        if charHash[charCode as usize] == true {
+            return false
+        }
+
+        charHash[charCode as usize] = true;
+        println!("Character: {}", c);
+    }
+
+    return true
+}
+
+// order of magnitude slower, less memory utilization
+pub fn is_unique_no_data_structures(string: &str) -> bool {
+    let mut unique = true;
+    for (i, c) in string.chars().enumerate() {
+        for sub_c in string.chars().skip(i + 1) {
+            if c == sub_c {
+                unique = false;
+                break
+            }
+        }
+    }
+
+    unique
 }
 
 #[cfg(test)]
@@ -11,7 +46,6 @@ pub mod unit_test {
 
     #[test]
     fn test_is_unique() {
-        println!('test');
         let test_cases = read_test_cases();
         for ref test_case in test_cases {
             let actual = is_unique(&test_case.input);
