1e3e9a9b8e17d4bcb53ade395cbf93fcd3f1cf02
Tanner Pearson <pearsontanner345@gmail.com>
2025-11-01 09:34:20 -0500

    add my solution for question 1

--- IsUnique/Tanner/benches/benchmark.rs
+++ IsUnique/Tanner/benches/benchmark.rs
@@ -1,22 +1,20 @@
 use std::hint::black_box;
 
-use criterion::Criterion;
 use criterion::criterion_group;
 use criterion::criterion_main;
+use criterion::Criterion;
 use is_unique::is_unique;
 use is_unique::test::read_test_cases;
 
-fn criterion_benchmark(c: &mut Criterion)
-{
+fn criterion_benchmark(c: &mut Criterion) {
     let test_cases = read_test_cases();
     c.bench_function("is_unique", |b| {
-         b.iter(|| {
-              for ref test_case in test_cases.clone()
-              {
-                  is_unique(black_box(&test_case.input));
-              }
-          })
-     });
+        b.iter(|| {
+            for ref test_case in test_cases.clone() {
+                is_unique(black_box(&test_case.input));
+            }
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
