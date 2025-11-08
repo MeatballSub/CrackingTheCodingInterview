b6dc3a6e9d221c0bf08128bcb74dd674225c28d1
mtwatson <watsonmichael919@gmail.com>
2025-11-08 10:59:36 -0600

    handled all same character case

--- PalindromePermutation/Mike/src/lib.rs
+++ PalindromePermutation/Mike/src/lib.rs
@@ -3,13 +3,24 @@ use std::collections::HashMap;
 pub mod test;
 
 pub fn palindrome_permutation(string: &str) -> bool {
-    if string.len() <= 1 {
+    if string.len() <= 1  {
+        println!("===============");
+        println!("short circuit evaluation");
+        println!("{:?}", string);
+        println!("===============");
         return true
     }
 
     let letters: Vec<_> = string.trim().to_lowercase().chars().filter(|c| !c.is_whitespace() && c.is_alphabetic()).collect();
+    let identical = letters.iter().all(|c| *c == letters[0]);
+
+    if identical {
+        return true;
+    }
+
     let mut letter_freq: HashMap<char, u32> = HashMap::new();
     let mut is_palindrome = false;
+
 
     letters.iter().for_each(|l| {
         *letter_freq.entry(*l).or_insert(0) += 1;
@@ -19,21 +30,25 @@ pub fn palindrome_permutation(string: &str) -> bool {
     let mut otherwise_symmetrical = false;
 
     letter_freq.into_iter().for_each(|(key, val)| {
-        println!("{:?}", key);
-        println!("{:?}", val);
-
         if val == 1{
             occurrence_of_one +=1
-        } if val == 2 {
+        } if val % 2 == 0 {
             otherwise_symmetrical = true
         } else {
             otherwise_symmetrical = false
         }
     });
    
+   
     if occurrence_of_one == 1 && otherwise_symmetrical == true {
         is_palindrome = true
     }
+    println!("===============");
+    println!("{:?}", string);
+    println!("{:?}", occurrence_of_one);
+    println!("{:?}", otherwise_symmetrical);
+    println!("{:?}", is_palindrome);
+    println!("===============");
     is_palindrome
     
 }
@@ -45,7 +60,7 @@ pub mod unit_test {
     use crate::test::read_test_cases;
 
     #[test]
-    fn test_urlify() {
+    fn test_palindrome_permutation() {
         let test_cases = read_test_cases();
         for ref test_case in test_cases {
             let actual = palindrome_permutation(&test_case.input);
