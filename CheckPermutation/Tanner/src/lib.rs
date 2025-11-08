2778b9dea4936a7cfb1f776491bb1bf0eabe4049
Tanner Pearson <pearsontanner345@gmail.com>
2025-11-08 10:14:38 -0600

    tanner's entry

--- CheckPermutation/Tanner/src/lib.rs
+++ CheckPermutation/Tanner/src/lib.rs
@@ -1,7 +1,31 @@
+use std::collections::HashMap;
+
 pub mod test;
 
 pub fn check_permutation(string1: &str, string2: &str) -> bool {
-    todo!("Implement this function")
+    if string1.len() != string2.len() {
+        return false;
+    }
+
+    let mut characters1 = HashMap::new();
+    let mut characters2 = HashMap::new();
+
+    for (char1, char2) in string1.chars().zip(string2.chars()) {
+        *characters1.entry(char1).or_insert(0) += 1;
+        *characters2.entry(char2).or_insert(0) += 1;
+    }
+
+    for (key, value1) in characters1.iter() {
+        let Some(value2) = characters2.get(key) else {
+            return false;
+        };
+
+        if *value1 != *value2 {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 #[cfg(test)]
