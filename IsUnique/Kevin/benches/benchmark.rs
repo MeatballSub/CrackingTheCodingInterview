e8b4d8aa0093f6e3ed0869c7d300dae1ade4c693
Kevin Day <kevinday.home@gmail.com>
2025-11-07 23:53:29 -0600

    Kevin's solutions

--- IsUnique/Kevin/benches/benchmark.rs
+++ IsUnique/Kevin/benches/benchmark.rs
@@ -3,17 +3,37 @@ use std::hint::black_box;
 use criterion::Criterion;
 use criterion::criterion_group;
 use criterion::criterion_main;
-use is_unique::is_unique;
+use is_unique::is_unique_hash_set;
+use is_unique::is_unique_no_data_structures;
+use is_unique::is_unique_seven_bit_ascii;
 use is_unique::test::read_test_cases;
 
 fn criterion_benchmark(c: &mut Criterion)
 {
     let test_cases = read_test_cases();
-    c.bench_function("is_unique", |b| {
+    c.bench_function("is_unique_hash_set", |b| {
          b.iter(|| {
               for ref test_case in test_cases.clone()
               {
-                  is_unique(black_box(&test_case.input));
+                  is_unique_hash_set(black_box(&test_case.input));
+              }
+          })
+     });
+
+    c.bench_function("is_unique_no_data_structures", |b| {
+         b.iter(|| {
+              for ref test_case in test_cases.clone()
+              {
+                  is_unique_no_data_structures(black_box(&test_case.input));
+              }
+          })
+     });
+
+    c.bench_function("is_unique_seven_bit_ascii", |b| {
+         b.iter(|| {
+              for ref test_case in test_cases.clone()
+              {
+                  is_unique_seven_bit_ascii(black_box(&test_case.input));
               }
           })
      });
--- IsUnique/Kevin/src/lib.rs
+++ IsUnique/Kevin/src/lib.rs
@@ -1,24 +1,73 @@
+use std::collections::HashSet;
+
 pub mod test;
 
-pub fn is_unique(string: &str) -> bool {
-    todo!("Implement this function")
+pub fn is_unique_hash_set(string: &str) -> bool
+{
+    let mut seen = HashSet::new();
+    for ch in string.chars()
+    {
+        if !seen.insert(ch)
+        {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn is_unique_no_data_structures(string: &str) -> bool
+{
+    let string_bytes = string.as_bytes();
+    for i in 0..string_bytes.len()
+    {
+        for j in i + 1..string_bytes.len()
+        {
+            if string_bytes.get(i).unwrap() == string_bytes.get(j).unwrap()
+            {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+pub fn is_unique_seven_bit_ascii(string: &str) -> bool
+{
+    let mut seen = [false; 128];
+    let string_bytes = string.as_bytes();
+    for ch in string_bytes
+    {
+        let flag = &mut seen[*ch as usize];
+        if *flag
+        {
+            return false;
+        }
+        *flag = true;
+    }
+    true
+}
+
+pub fn is_unique(string: &str) -> bool
+{
+    // is_unique_hash_set(string)
+    //is_unique_no_data_structures(string)
+    is_unique_seven_bit_ascii(string)
 }
 
 #[cfg(test)]
-pub mod unit_test {
+pub mod unit_test
+{
     use super::*;
     use crate::test::read_test_cases;
 
     #[test]
-    fn test_is_unique() {
+    fn test_is_unique()
+    {
         let test_cases = read_test_cases();
-        for ref test_case in test_cases {
+        for ref test_case in test_cases
+        {
             let actual = is_unique(&test_case.input);
-            assert_eq!(
-                actual, test_case.answer,
-                "input: '{}', expected: {}, actual: {}",
-                test_case.input, test_case.answer, actual
-            );
+            assert_eq!(actual, test_case.answer, "input: '{}', expected: {}, actual: {}", test_case.input, test_case.answer, actual);
         }
     }
 }
