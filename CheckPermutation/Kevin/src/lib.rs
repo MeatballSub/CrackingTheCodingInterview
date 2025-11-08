83f15335fbdc5dbc3db2c6dc6ee3c17fd0d1665b
Kevin Day <kevinday.home@gmail.com>
2025-11-08 01:27:58 -0600

    Kevin - CheckPermutation

--- CheckPermutation/Kevin/src/lib.rs
+++ CheckPermutation/Kevin/src/lib.rs
@@ -1,24 +1,48 @@
 pub mod test;
 
-pub fn check_permutation(string1: &str, string2: &str) -> bool {
-    todo!("Implement this function")
+pub fn check_permutation(string1: &str, string2: &str) -> bool
+{
+    if string1.len() != string2.len()
+    {
+        return false;
+    }
+
+    let mut frequencies = [0usize; 128];
+    let string_bytes = string1.as_bytes();
+    for ch in string_bytes
+    {
+        frequencies[*ch as usize] += 1;
+    }
+
+    let string_bytes = string2.as_bytes();
+    for ch in string_bytes
+    {
+        if frequencies[*ch as usize] == 0
+        {
+            return false;
+        }
+        frequencies[*ch as usize] -= 1;
+    }
+
+    frequencies.iter().all(|count| *count == 0)
 }
 
 #[cfg(test)]
-pub mod unit_test {
+pub mod unit_test
+{
     use super::*;
     use crate::test::read_test_cases;
 
     #[test]
-    fn test_check_permutation() {
+    fn test_check_permutation()
+    {
         let test_cases = read_test_cases();
-        for ref test_case in test_cases {
+        for ref test_case in test_cases
+        {
             let actual = check_permutation(&test_case.input1, &test_case.input2);
-            assert_eq!(
-                actual, test_case.answer,
-                "input: '{},{}', expected: {}, actual: {}",
-                test_case.input1, test_case.input2, test_case.answer, actual
-            );
+            assert_eq!(actual, test_case.answer,
+                       "input: '{},{}', expected: {}, actual: {}",
+                       test_case.input1, test_case.input2, test_case.answer, actual);
         }
     }
 }
--- CheckPermutation/Kevin/src/test.rs
+++ CheckPermutation/Kevin/src/test.rs
@@ -4,16 +4,16 @@ use std::path::Path;
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize, Debug)]
-pub struct TestCase {
+pub struct TestCase
+{
     pub input1: String,
     pub input2: String,
     pub answer: bool,
 }
 
-pub fn read_test_cases() -> Vec<TestCase> {
-    let data_path = Path::new("../TestData/data.json")
-        .canonicalize()
-        .expect("Invalid path: '../TestData/data.json'");
+pub fn read_test_cases() -> Vec<TestCase>
+{
+    let data_path = Path::new("../TestData/data.json").canonicalize().expect("Invalid path: '../TestData/data.json'");
     let open_error_msg = format!("error opening file: {}", data_path.display());
     let parse_error_msg = format!("error parsing file: {}", data_path.display());
     let file = File::open(data_path).expect(&open_error_msg);
