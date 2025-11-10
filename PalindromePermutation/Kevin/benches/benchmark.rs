966e77f7b64aa2633a621b5d5ec809e392be94bb
Kevin Day <kevinday.home@gmail.com>
2025-11-09 22:16:30 -0600

    Kevin - optimize Palindrome Permutation

--- PalindromePermutation/Kevin/benches/benchmark.rs
+++ PalindromePermutation/Kevin/benches/benchmark.rs
@@ -1,23 +1,56 @@
 use std::hint::black_box;
+use std::time::Duration;
 
 use criterion::Criterion;
 use criterion::criterion_group;
 use criterion::criterion_main;
-use palindrome_permutation::palindrome_permutation;
+use palindrome_permutation::palindrome_permutation_kevin_1;
+use palindrome_permutation::palindrome_permutation_kevin_2;
+use palindrome_permutation::palindrome_permutation_kevin_3;
+use palindrome_permutation::palindrome_permutation_mike;
 use palindrome_permutation::test::read_test_cases;
 
 fn criterion_benchmark(c: &mut Criterion)
 {
     let test_cases = read_test_cases();
-    c.bench_function("palindrome_permutation", |b| {
+    c.bench_function("palindrome_permutation_kevin_1", |b| {
          b.iter(|| {
               for test_case in &test_cases
               {
-                  palindrome_permutation(black_box(&test_case.input));
+                  palindrome_permutation_kevin_1(black_box(&test_case.input));
+              }
+          })
+     });
+    c.bench_function("palindrome_permutation_kevin_2", |b| {
+         b.iter(|| {
+              for test_case in &test_cases
+              {
+                  palindrome_permutation_kevin_2(black_box(&test_case.input));
+              }
+          })
+     });
+    c.bench_function("palindrome_permutation_kevin_3", |b| {
+         b.iter(|| {
+              for test_case in &test_cases
+              {
+                  palindrome_permutation_kevin_3(black_box(&test_case.input));
+              }
+          })
+     });
+    c.bench_function("palindrome_permutation_mike", |b| {
+         b.iter(|| {
+              for test_case in &test_cases
+              {
+                  palindrome_permutation_mike(black_box(&test_case.input));
               }
           })
      });
 }
 
-criterion_group!(benches, criterion_benchmark);
+// criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(5000).measurement_time(Duration::from_secs(10)).warm_up_time(Duration::from_secs(6));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
--- PalindromePermutation/Kevin/src/lib.rs
+++ PalindromePermutation/Kevin/src/lib.rs
@@ -1,22 +1,94 @@
+use std::collections::HashMap;
+
 pub mod test;
 
-pub fn palindrome_permutation(string: &str) -> bool
+pub fn palindrome_permutation(string: &str) -> bool { palindrome_permutation_kevin_3(string) }
+
+pub fn palindrome_permutation_kevin_1(string: &str) -> bool
 {
-    let mut frequencies = [0usize; 128];
+    let mut frequencies = [0usize; 26];
     let string_bytes = string.as_bytes();
     for ch in string_bytes
     {
         // 32 is the mask to set the bit to do a to lower
-        // 'a' is ascii code 61
-        // 'z' is ascii code 122
         let ch_index = ch | 32;
-        if 60 < ch_index && ch_index < 123
+        if b'a' <= ch_index && ch_index <= b'z'
         {
+            frequencies[(ch_index - b'a') as usize] += 1;
+        }
+    }
+
+    frequencies.iter().filter(|c| *c % 2 == 1).count() <= 1
+}
+
+pub fn palindrome_permutation_kevin_2(string: &str) -> bool
+{
+    let mut frequencies = [0usize; 26];
+    let string_bytes = string.as_bytes();
+    for ch in string_bytes
+    {
+        if ch.is_ascii_alphabetic()
+        {
+            let ch_index = ch.to_ascii_lowercase() - b'a';
             frequencies[ch_index as usize] += 1;
         }
     }
 
     frequencies.iter().filter(|c| *c % 2 == 1).count() <= 1
+}
+
+pub fn palindrome_permutation_kevin_3(string: &str) -> bool
+{
+    let mut odd_even_bits = 0u32;
+    let string_bytes = string.as_bytes();
+    for ch in string_bytes
+    {
+        let ch_index = ch | 32;
+        if b'a' <= ch_index && ch_index <= b'z'
+        {
+            odd_even_bits ^= 1 << (ch_index - b'a');
+        }
+    }
+
+    odd_even_bits & odd_even_bits.wrapping_sub(1) == 0
+}
+
+pub fn palindrome_permutation_mike(string: &str) -> bool
+{
+    if string.len() <= 1
+    {
+        return true;
+    }
+
+    let letters: Vec<_> = string.trim().to_lowercase().chars().filter(|c| !c.is_whitespace() && c.is_alphabetic()).collect();
+    let identical = letters.iter().all(|c| *c == letters[0]);
+
+    if identical
+    {
+        return true;
+    }
+
+    let mut letter_freq: HashMap<char, u32> = HashMap::new();
+    let mut is_palindrome = false;
+
+    letters.iter().for_each(|l| {
+                      *letter_freq.entry(*l).or_insert(0) += 1;
+                  });
+
+    let mut uneven_frequency = 0;
+    letter_freq.into_iter().for_each(|(_key, val)| {
+                               if val % 2 != 0
+                               {
+                                   uneven_frequency += 1
+                               }
+                           });
+
+    if uneven_frequency <= 1
+    {
+        is_palindrome = true
+    }
+
+    is_palindrome
 }
 
 #[cfg(test)]
@@ -26,7 +98,7 @@ pub mod unit_test
     use crate::test::read_test_cases;
 
     #[test]
-    fn test_urlify()
+    fn test_palindrome_permutation()
     {
         let test_cases = read_test_cases();
         for ref test_case in test_cases
