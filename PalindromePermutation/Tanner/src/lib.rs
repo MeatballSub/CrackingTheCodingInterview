b7c9a6fcf5338c71ef9a6c67626f0bfe160b6680
Tanner Pearson <pearsontanner345@gmail.com>
2025-11-14 17:34:38 -0600

    tanner palindrome solution

--- PalindromePermutation/Tanner/src/lib.rs
+++ PalindromePermutation/Tanner/src/lib.rs
@@ -1,7 +1,30 @@
+use std::collections::HashMap;
+
 pub mod test;
 
 pub fn palindrome_permutation(string: &str) -> bool {
-    todo!("Implement this function")
+    if string.is_empty() {
+        return true;
+    }
+
+    let mut char_map: HashMap<char, usize> = HashMap::new();
+
+    for character in string.to_lowercase().chars() {
+        if !character.is_alphabetic() {
+            continue;
+        }
+        *char_map.entry(character).or_insert(0) += 1;
+    }
+
+    let mut num_odds = 0;
+
+    for num in char_map.values() {
+        if num % 2 != 0 {
+            num_odds += 1;
+        }
+    }
+
+    num_odds % 2 != 0
 }
 
 #[cfg(test)]
