9bc6a82722ee4f02553e49c697dee7771bca32e6
mtwatson <watsonmichael919@gmail.com>
2025-11-14 09:33:18 -0600

    mtw first crack at is permutation. tests pass.

--- CheckPermutation/Mike/src/lib.rs
+++ CheckPermutation/Mike/src/lib.rs
@@ -1,7 +1,34 @@
+use std::{collections::HashMap};
+
 pub mod test;
 
+fn calc_freq(string: &str) -> HashMap<char, u32> {
+    let mut letter_freq: HashMap<char, u32> = HashMap::new();
+    let letters: Vec<_> = string
+        .trim()
+        .to_lowercase()
+        .chars()
+        .collect();
+
+    letters.iter().for_each(|l| {
+        *letter_freq.entry(*l).or_insert(0) += 1;
+    });
+
+    letter_freq
+}
+
 pub fn check_permutation(string1: &str, string2: &str) -> bool {
-    todo!("Implement this function")
+    let string_1_vec: Vec<_> = string1.chars().collect();
+    let string_2_vec: Vec<_> = string2.chars().collect();
+
+    if string_1_vec.len() != string_2_vec.len() {
+        return false
+    }
+
+    let mut s1_letter_freq: HashMap<char, u32> = calc_freq(string1);
+    let mut s2_letter_freq: HashMap<char, u32> = calc_freq(string2);
+
+    s1_letter_freq == s2_letter_freq
 }
 
 #[cfg(test)]
