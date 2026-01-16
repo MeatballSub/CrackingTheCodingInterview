cc1e13cfcd8390d2e4f389e678f656abe3662a61
Kevin Day <kevinday.home@gmail.com>
2026-01-16 17:27:34 -0600

    Fix RotateMatrix for in-place

--- RotateMatrix/Kevin/src/lib.rs
+++ RotateMatrix/Kevin/src/lib.rs
@@ -7,7 +7,7 @@ pub mod test;
 // ↱→↴
 // ↑↻↓
 // ⬑←↲
-pub fn rotate_matrix(matrix: &[Vec<usize>]) -> Vec<Vec<usize>> {
+pub fn rotate_matrix(matrix: &mut [Vec<usize>]) -> &[Vec<usize>] {
     todo!("Implement this function")
 }
 
@@ -20,11 +20,12 @@ pub mod unit_test {
     fn test_rotate_matrix() {
         let test_cases = read_test_cases();
         for ref mut test_case in test_cases {
-            let actual = rotate_matrix(&test_case.matrix);
+            let original = test_case.matrix.clone();
+            let actual = rotate_matrix(&mut test_case.matrix);
             assert_eq!(
                 actual, test_case.answer,
                 "input: '{:?}', expected: {:?}, actual: {:?}",
-                test_case.matrix, test_case.answer, actual
+                original, test_case.answer, actual
             );
         }
     }
