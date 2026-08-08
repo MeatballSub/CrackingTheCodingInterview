a27e1cd76601efa98c5429b28611076f2e557a7e
mtwatson <watsonmichael919@gmail.com>
2026-08-08 07:21:38 -0500

    solution for three in one

--- ThreeInOne/Mike/src/lib.rs
+++ ThreeInOne/Mike/src/lib.rs
@@ -16,23 +16,51 @@ pub struct FixedMultiStack {
 
 impl FixedMultiStack {
     pub fn new(stack_capacity: usize) -> Self {
-        todo!()
+        let placeholder = 0;
+        Self {
+            stack_capacity,
+            values:vec![placeholder; stack_capacity * 3],
+            sizes: [0; 3],
+        }
     }
 
     pub fn push(&mut self, stack_num: usize, value: i32) -> Result<(), StackError> {
-        todo!()
+        if self.is_full(stack_num) {
+            return Err(StackError::Full);
+        }
+        let next_free = self.next_free_slot(stack_num);
+        self.values[next_free] = value;
+        self.sizes[stack_num] += 1;
+        Ok(())
     }
 
     pub fn pop(&mut self, stack_num: usize) -> Option<i32> {
-        todo!()
+        if self.is_empty(stack_num) {
+            return None;
+        }
+        let target_val = self.next_free_slot(stack_num) - 1;
+        self.sizes[stack_num] -= 1;
+        Some(self.values[target_val])
     }
 
     pub fn peek(&self, stack_num: usize) -> Option<i32> {
-        todo!()
+        if self.is_empty(stack_num) {
+            return None;
+        }
+        let target_val = self.next_free_slot(stack_num) - 1;
+        Some(self.values[target_val])
     }
 
     pub fn is_empty(&self, stack_num: usize) -> bool {
-        todo!()
+        self.sizes[stack_num] == 0
+    }
+
+    pub fn is_full(&self, stack_num: usize) -> bool {
+        self.sizes[stack_num] >= self.stack_capacity
+    }
+
+    fn next_free_slot(&self, stack_number: usize) -> usize {
+        stack_number * self.stack_capacity + self.sizes[stack_number]
     }
 }
 
